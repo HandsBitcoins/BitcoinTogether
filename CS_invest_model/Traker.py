4e8370dc51328d44625d45b2c246bc377423ce28
Flkalas <rectifying@gmail.com>
2015-04-10 12:06:01 +0900

    5개 사이트 완성.
    시간 왜 이리 느려.

--- CS_invest_model/Traker.py
+++ CS_invest_model/Traker.py
@@ -11,45 +11,57 @@ import random
 # bitfinex
 # lakebtc
 
+timeStamps = []
+timeStamps.append(time.time())
+
 # https://data.btcchina.com/data/ticker?market=all
 params = {'market': 'all'}
 parameter = urllib.urlencode(params)
-
-#GET https://api.korbit.co.kr/v1/ticker/detailed                
+             
 connection = httplib.HTTPSConnection("data.btcchina.com")
 connection.request("GET","/data/ticker",parameter)
 response = connection.getresponse()
 jsonResponse = response.read()
 infoPrices = json.loads(jsonResponse)
 print infoPrices
+timeStamps.append(time.time())
 
-# https://www.okcoin.com/api/klineData.do?marketFrom=34&type=2&limit=1000&coinVol=1
-# https://www.okcoin.com/api/klineData.do?marketFrom=34&type=2&since=1428623100000&coinVol=1
-# https://www.okcoin.com/api/klineData.do?marketFrom=34&type=2&since=1428624000000&coinVol=1
-# flooredTiem = math.floor(time.time()/100)*100000
-# strTime = "%012d" % flooredTiem 
-# print strTime
-# #1428623100000
-# #1428624281000
-# #1428624000000
-# params = {'marketFrom': '34',
-#           'type': '2',
-#           'since': strTime,
-#           'coinVol': '1'}
-# parameter = urllib.urlencode(params)
-# 
-# connection = httplib.HTTPSConnection("www.okcoin.com")
-# connection.request("GET","/api/klineData.do",parameter)
-# response = connection.getresponse()
-# infoPrices = response.read()
-# print infoPrices
-
-# https://api.huobi.com/staticmarket/td_btc.html?0.7971218014135957
-connection = httplib.HTTPSConnection("api.huobi.com")
-connection.request("GET","/staticmarket/td_btc.html",str(random.random()))
+# https://www.okcoin.com/api/ticker.do?ok=1
+connection = httplib.HTTPSConnection("www.okcoin.com")
+connection.request("GET","/api/ticker.do?ok=1")#, parameter)
 response = connection.getresponse()
-infoPrices = response.read()
-
+jsonResponse = response.read()
+infoPrices = json.loads(jsonResponse)
 print infoPrices
+timeStamps.append(time.time())
 
+# http://api.huobi.com/staticmarket/ticker_btc_json.js 
+connection = httplib.HTTPSConnection("api.huobi.com")
+connection.request("GET","/staticmarket/ticker_btc_json.js")
+response = connection.getresponse()
+jsonResponse = response.read()
+infoPrices = json.loads(jsonResponse)
+print infoPrices
+timeStamps.append(time.time())
 
+# https://api.bitfinex.com/v1/pubticker/:symbol
+connection = httplib.HTTPSConnection("api.bitfinex.com")
+connection.request("GET","/v1/pubticker/btcusd")
+response = connection.getresponse()
+jsonResponse = response.read()
+infoPrices = json.loads(jsonResponse)
+print infoPrices
+timeStamps.append(time.time())
+
+# https://www.LakeBTC.com/api_v1/ticker
+connection = httplib.HTTPSConnection("www.LakeBTC.com")
+connection.request("GET","/api_v1/ticker")
+response = connection.getresponse()
+jsonResponse = response.read()
+infoPrices = json.loads(jsonResponse)
+print infoPrices
+timeStamps.append(time.time())
+
+for i in range(len(timeStamps)-1):
+    print timeStamps[i+1] - timeStamps[i]
+    
