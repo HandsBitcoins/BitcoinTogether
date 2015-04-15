b5f82b02d8bbb563889c0028227ee7188271bfe6
Flkalas <rectifying@gmail.com>
2015-04-16 04:34:38 +0900

    통계학자 작성 시작.
    방법에 대해서 약간 고민해봐야할 듯.
    하락 후 상승의 비를 기록해서 쓰는 것도 나름 괜찮을 듯.

--- CS_invest_model/ClosimStatistician.py
+++ CS_invest_model/ClosimStatistician.py
@@ -1,14 +1,34 @@
-
 class closimStatistician(object):
 	def __init__(self):
 		pass
 
+    def init(self):
+        self.isBuy = False
+        self.nowDown = 0.0
+        self.avgDown = 0.0
+        self.stdDown = 0.0
+                
+        self.isDesending = False
+        self.streamData = []
+                
+    def connectDataBase(self):
+        pass
+        
+    def proceedStep(self,priceAsk):
+        self.streamData.append(priceAsk)
 
-
-
-def checkIsPeak(priceQuote,pricePrev):
-	return pricePrev > priceQuote
-	
-def checkIsFloor(priceQuote,pricePrev):
-	return pricePrev < priceQuote
-	
+        if isDesending:
+            if self.streamData[-1] > self.streamData[-2] :
+                self.isBuy = True
+                self.nowDown = self.streamData[0] - self.streamData[-2]
+                
+                
+        
+            
+    def getStdDown(self):
+        pass
+    
+    
+    
+    def getAvgDown(self):
+        pass
